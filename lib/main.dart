cb670db1ae4c1b347f87cb643a5a6fec997df2e3
EmilyBaxter <emily.g.baxter@btinternet.com>
2024-02-13 16:44:45 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -99,6 +99,5 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-
   }
 }
