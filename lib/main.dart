c96c9851784014eb28914d9f85e269f9041b9bbc
aGamer106 <agamer106@yahoo.com>
2024-02-15 00:15:53 +0000

    added mobile camera movement controls which completely work; the switching between the front-facing camera and back-facing camera are also enabled via the top-right corner icon;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+//modified the entire starter template, just as a heads-up
+//new structure with the latest updates can be found below:
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,13 +26,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  //we need a controller for our Camera to actually work; I named it cameraController so that it's self-explanatory from the name
+  MobileScannerController cameraController = MobileScannerController();
+
+  // get barcode => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('QRScanner'),
+        actions: [IconButton(onPressed: (){cameraController.switchCamera();
+        },
+            icon: const Icon(Icons.camera_rear_outlined))] //actions performed by the camera - this basically allows the user to switch between front-facing camera to back-facing camera; The icon takes a constant value here for now.
       ),
-      body: Container(),
+      body: MobileScanner(
+          allowDuplicates: false,
+          controller: cameraController, //pass the controller to the object itself here in order for it to use
+          onDetect: (barcode, args) {
+            debugPrint('Barcode Found! ' + barcode.rawValue!);
+      })
     );
   }
 }
