2989dfd3905cc9be5cfcd489614a3ba0c33a1f43
aGamer106 <agamer106@yahoo.com>
2024-02-20 11:30:47 +0000

    added user class

---
+++ lib/user.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+
+
+
+class User {
+
+  //object properties
+  final String firstName;
+  final String lastName;
+  final String userEmail;
+
+  //object constructor here
+  User(
+      {required this.firstName,
+      required this.lastName,
+      required this.userEmail});
+
+
+  //map the data for the object and retrieve it then from the database down here
+  factory User.fromMap(Map<dynamic, dynamic> data) {
+    return User(
+        firstName: data['First_Name'] ?? '',
+        lastName: data['Last_Name'] ?? '',
+        userEmail: data['User_Email'] ?? ''
+    );
+  }
+
+
+
+
+}
